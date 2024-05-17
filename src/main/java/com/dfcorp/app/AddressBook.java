8a14fb20f5b5019a5572e5961e0df6482cec6d9f
fabiodplacerda <lacerda96@gmail.com>
2024-05-17 11:39:26 +0100

    REFACTOR: Refactor AddressBook class for improved readability and consistency

--- src/main/java/com/dfcorp/app/AddressBook.java
+++ src/main/java/com/dfcorp/app/AddressBook.java
@@ -8,24 +8,25 @@ import java.util.ArrayList;
 public class AddressBook {
     final private ArrayList<Contact> contactList = new ArrayList<>();
 
-    public AddressBook() {
-    }
-
+    // Method that adds a contact to the AddressBook
     public void addContact(Contact contact) {
         if (Validator.isContactNull(contact)) throw new IllegalArgumentException("Invalid Contact");
-        if (isDuplicate(contact))
+        if (isDuplicate(contact.getPhoneNumber(), contact.getEmailAddress()))
             throw new IllegalArgumentException("Phone number or Email, already exist in the address book. Please provide unique phone number or email");
 
         this.contactList.add(contact);
     }
 
+    // Method that removes a contact from the AddressBook
     public void removeContact(Contact contact) {
         this.contactList.remove(contact);
     }
 
+    // Method that edits a contact in the AddressBook, iterating over the contactList
     public void editContact(Contact contact, String newName, String newPhoneNumber, String newEmailAddress) {
-        if (Validator.isNull(newName) || Validator.isNull(newPhoneNumber) || Validator.isNull(newEmailAddress))
-            throw new IllegalArgumentException("Invalid values");
+        Validator.validateName(newName);
+        Validator.validatePhoneNumber(newPhoneNumber);
+        Validator.validateEmailAddress(newEmailAddress);
         if (isDuplicate(newPhoneNumber, newEmailAddress))
             throw new IllegalArgumentException("The contacts in address book must be unique");
         for (Contact contactInArrayList : contactList) {
@@ -38,20 +39,23 @@ public class AddressBook {
 
     }
 
+    // Method that searches for a contact using the name associated with that contact (case-insensitive)
     public ArrayList<Contact> searchContact(String name) {
         ArrayList<Contact> contactsToReturn = new ArrayList<>();
         for (Contact contact : contactList) {
-            if (contact.getName().equals(name)) {
+            if (contact.getName().toLowerCase().equals(name.toLowerCase())) {
                 contactsToReturn.add(contact);
             }
         }
         return contactsToReturn;
     }
 
+    // Method that returns the full contactList
     public ArrayList<Contact> getContactList() {
         return contactList;
     }
 
+    // Method that checks if there are phone number or email address already exists in the AddressBook
     private boolean isDuplicate(String phoneNumber, String emailAddress) {
         for (Contact contactInList : contactList) {
             if (contactInList.getPhoneNumber().equals(phoneNumber) || contactInList.getEmailAddress().equals(emailAddress)) {
@@ -60,10 +64,4 @@ public class AddressBook {
         }
         return false;
     }
-
-    private boolean isDuplicate(Contact contact) {
-        return isDuplicate(contact.getPhoneNumber(), contact.getEmailAddress());
-    }
-
-
 }
