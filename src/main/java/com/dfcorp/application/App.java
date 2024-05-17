593bdb05580a2a8e9244ab4b01858935f9473c22
fabiodplacerda <lacerda96@gmail.com>
2024-05-17 13:33:47 +0100

    REFACTOR: App.java for improved user experience

--- src/main/java/com/dfcorp/application/App.java
+++ src/main/java/com/dfcorp/application/App.java
@@ -18,8 +18,9 @@ public class App {
 
 
         while (isRunning) {
+            Message.print("-----------Address Book-----------\n---------------Menu---------------");
             Message.print("What action would you like to perform? (Choose 1-5)\n1. Add a Contact\n2. Remove a Contact\n3. Edit a Contact\n4. Search a Contact by name\n5. Get all Contacts in this address book\nQ. Exit program");
-            String output = UserInputs.getCommand(scanner.nextLine());
+            String output = UserInputs.getCommand(scanner);
             switch (output) {
                 case "1":
                     Message.print("You are adding a new contact please provide a valid Name, Phone number and Email Address");
@@ -66,7 +67,7 @@ public class App {
                     break;
                 case "4":
                     Message.print("You are searching for a contact please provide the name of that contact");
-                    String contactByName = scanner.nextLine();
+                   String contactByName = UserInputs.getName(scanner);
                     Message.printSearchResults(contactByName, addressBook);
                     break;
                 case "5":
